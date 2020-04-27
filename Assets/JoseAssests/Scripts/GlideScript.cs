5af8f186eb0e9b87fa34250a00662b4602657ee2
josemg2556 <56130895+josemg2556@users.noreply.github.com>
2020-04-27 12:57:04 -0400

    new things added
    new scene new script new playing field

---
+++ Assets/JoseAssests/Scripts/GlideScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlideScript : MonoBehaviour
+{
+   public float speed = 30.0f;
+    
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("charcter script added to:" + gameObject);
+
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        {
+
+            transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+            transform.position += transform.forward * Time.deltaTime * 90.0f;
+            speed -= transform.forward.y * 50.0f;
+
+
+
+
+
+
+
+
+        }
+
+
+        if (speed < 35.0f)
+        {
+            speed = 35.0f;
+        }
+
+
+        
+       
+    }
+
+
+       
+}
